32a15068011e3b6e7526d81f2ffd28bd6483aa27
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-06-24 15:48:44 +0530

    Add project files.

---
+++ ProductClass/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductClass
+{
+    public class Product
+    {
+        int productcode;
+        string productname;
+        double productprice;
+
+        public int ProductCode
+        {
+            set { productcode = value; }
+            get { return productcode; }
+        }
+        public string Productname
+        {
+            set { productname = value; }
+            get { return productname; }
+        }
+        public double ProductPrice
+        {
+            set { productprice = value; }
+            get { return productprice; }
+        }
+        public string putdata()
+        {
+            return $" date is{productcode}-{productname}-{productname}.";
+        }
+    }
+}
---
+++ ProductClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // property initializer
+            Product product = new Product();
+            product.ProductCode = 45;
+            product.Productname = "Italian";
+            product.ProductPrice = 500;
+
+            Console.WriteLine($"Product Details is {product.ProductCode}:{product.Productname}:{product.ProductPrice}");
+            //// object initializer
+            Product product1 = new Product() { ProductCode=45,Productname="Italian",ProductPrice=500};
+
+            Console.WriteLine($"Product Details is{product1.ProductCode},{product1.Productname},{product1.ProductPrice}");
+
+        }
+    }
+}
